28f9b3377ab25b053e55e262bfa81bc5916c54c0
Shikhar Gaur <shikhargaur2008@gmail.com>
2025-01-01 12:57:03 +0530

    fixed board commit

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -47,9 +47,59 @@ const Board = () => {
   }
 
   return (
-    <div>
-      <h1>{selectedBoard?.data?.title}</h1>
-      {/* Add more details about the board here */}
+    <div className="w-screen pt-16 flex flex-col justify-center">
+      <div className="w-full h-12 flex px-20 justify-between items-center">
+        <span className="text-4xl font-bold">{selectedBoard?.data?.title}</span>
+
+        <div className="h-full flex items-center justify-center gap-2">
+          <div className="flex items-center justify-center h-full w-12 hover:bg-zinc-200 rounded-full p-2">
+            <img
+              src="/Inbox-Tray-2--Streamline-Core.svg"
+              className="h-2/3 w-2/3"
+              alt=""
+            />
+          </div>
+          <div className="flex items-center justify-center h-full w-12 hover:bg-zinc-200 rounded-full p-2">
+            <img src="/ellypsis.svg" className="h-full w-full" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <span className="text-zinc-500 px-20 mt-4">
+        {selectedBoard?.data?.pins?.length} Pins
+      </span>
+
+      <div className="w-full px-20 mt-4 h-10 flex items-center">
+        <div className="collaborators flex items-center justify-between h-full relative w-20">
+          {/* User div */}
+          <div className="h-full w-10 border-2 border-white bg-red-200 rounded-full z-[1]"></div>
+
+          {/* Collaborators divs */}
+          {selectedBoard?.data?.collaborators.map((collaborator, index) => (
+            <div
+              key={index}
+              className={`h-full w-10 border-2 border-white bg-blue-200 rounded-full z-[${
+                index + 2
+              }] absolute ml-[${(index + 1) * 1.8}rem]`}
+            ></div>
+          ))}
+
+          {/* Last div with image */}
+          <div
+            className={`h-full w-10 border-2 border-white bg-zinc-200 rounded-full hover:bg-zinc-300 z-[${
+              selectedBoard?.data?.collaborators.length + 2
+            }] absolute ml-[${
+              (selectedBoard?.data?.collaborators.length + 1) * 1.8
+            }rem] p-2`}
+          >
+            <img
+              src="/User-Add-Plus--Streamline-Core.svg"
+              className="w-full h-full"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
